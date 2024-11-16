6fe0fcddff13e2370207299cb09166e2a97f3b76
moonboylabsxyz <moonboylabs@proton.me>
2024-11-16 20:07:39 +0000

    we can rebuild him

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -5,13 +5,21 @@ export default defineConfig({
     outDir: "dist",
     sourcemap: true,
     clean: true,
-    format: ["esm"], // Ensure you're targeting CommonJS
+    format: ["esm"],
+    noExternal: ["fastembed", "unique-names-generator"],
     external: [
-        "dotenv", // Externalize dotenv to prevent bundling
-        "fs", // Externalize fs to use Node.js built-in module
-        "path", // Externalize other built-ins if necessary
-        "http",
-        "https",
-        // Add other modules you want to externalize
+        "@ai-sdk/*",
+        "@anthropic-ai/sdk",
+        "ai",
+        "anthropic-vertex-ai",
+        "gaxios",
+        "glob",
+        "js-sha1",
+        "ollama-ai-provider",
+        "openai",
+        "tiktoken",
+        "tinyld",
+        "together-ai",
+        "uuid"
     ],
 });
--- packages/core/types/index.d.ts
+++ packages/core/types/index.d.ts
@@ -1,4 +1,23 @@
 declare global {
     interface Buffer extends Uint8Array {}
 }
+
+declare module "fastembed" {
+    export class FlagEmbedding {
+        static init(options: { cacheDir: string }): Promise<FlagEmbedding>;
+        queryEmbed(text: string): Promise<number[]>;
+    }
+}
+
+declare module "unique-names-generator" {
+    export interface Config {
+        dictionaries: string[][];
+        length?: number;
+        separator?: string;
+        style?: "capital" | "lowerCase" | "upperCase";
+    }
+    export const names: string[];
+    export function uniqueNamesGenerator(config: Config): string;
+}
+
 export {};
